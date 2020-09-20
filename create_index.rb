c4d862792ef6d7a58a9ae93d242a24af6586314f
ISHIKAWA Mutsumi <ishikawa@hanzubon.jp>
2020-09-20 17:02:48 +0900

    add error handling and usage message

--- create_index.rb
+++ create_index.rb
@@ -3,6 +3,19 @@
 
 require "./common"
 
+def usage
+  puts <<"__EOM__"
+Usage: #{$0}
+You must put KEN_ALL.CSV csv file in same directory
+__EOM__
+end
+
 idx = NgramIndex.new
-idx.create
+begin
+  idx.create
+rescue ArgumentError => e
+  puts "Error in creating index file: #{e.message}"
+  usage
+  exit 1
+end
 idx.save
--- search.rb
+++ search.rb
@@ -3,6 +3,26 @@
 
 require "./common"
 
+def usage
+  puts <<"__EOM__"
+Usage: #{$0} search_word
+You must run create_index.rb to create search index file before using this command.
+__EOM__
+end
+
+if ARGV.length < 1
+  usage
+  exit 1
+end
+
 idx = NgramIndex.new
-idx.load
-idx.search_print("渋谷")
+
+begin
+  idx.load
+rescue ArgumentError => e
+  puts "Error in loading index file: #{e.message}"
+  usage
+  exit 1
+end
+
+idx.search_print(ARGV[0])
